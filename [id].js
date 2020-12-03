dc1d6ba82d0a7ffeca00e3bc577dc6ff66c58c56
Ahmet ÇAKIR <51996326+aahmetcakir@users.noreply.github.com>
2020-12-03 18:23:07 +0300

    Add files via upload

---
+++ [id].js
@@ -0,0 +1,45 @@
+import unfetch from "isomorphic-unfetch"
+import slug from "slug"
+import Card from "../../components/card"
+
+function characterDetail({ character }) {
+
+    return <div>
+        <Card
+            name={character.name}
+            gender={character.gender}
+            species={character.species}
+            image={character.image}
+            status={character.status}
+            location={character.location} />
+        </div>
+}
+
+export async function getStaticPaths() {
+    const data = await unfetch("https://rickandmortyapi.com/api/character/")
+    const characters = await data.json()
+
+    const paths = characters.results.map(character => {
+        return { params: { id: `${slug(character.name)}-${character.id}`}}} )
+    return {
+        paths,
+        fallback:false
+  }
+}
+
+export async function getStaticProps( { params } ) {
+    const ids = params.id.split('-').slice(-1)[0]
+    const data = await unfetch(`https://rickandmortyapi.com/api/character/${ids}`)
+    const character = await data.json()
+    console.log(`\n\nname : ${character.name}\nstatus: ${character.status}\nspecies: ${character.species}\ngender: ${character.gender}\nlocation: ${character.location.name}`)
+    return {
+        props: {
+            character
+        }
+    }
+}
+
+
+export default characterDetail
+
+
